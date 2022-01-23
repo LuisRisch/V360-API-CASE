8a3b570c408f1da5c7d98ebdc5d491a07acc5ecc
Luis Felipe Risch <lfr20@inf.ufpr.br>
2022-01-23 09:00:49 -0300

    initial commit

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+/* Error handling route */
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+
 app.listen(8080);
+
